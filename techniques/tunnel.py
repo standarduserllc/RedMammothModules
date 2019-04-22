6f6e09f4e707ea64dc21deba4f223bf935199a71
standarduserworkbench <22005871+standarduserllc@users.noreply.github.com>
2019-04-22 13:34:19 -0500

    Create tunnel.py

---
+++ techniques/tunnel.py
@@ -0,0 +1,112 @@
+
+#!/usr/bin/env python
+#
+# Copyright (C) 2016 David Evenden (@jedimammoth)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#This is a modified version of the Python Proxy found here: http://voorloopnul.com/blog/a-python-proxy-in-less-than-100-lines-of-code/
+
+import socket
+import select
+import time
+import sys
+
+
+buffer_size = 4096
+delay = 0.0001
+forward_to = ('10.10.10.100', 443)
+
+class Forward:
+    def __init__(self):
+        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self, host, port):
+        try:
+            self.forward.connect((host, port))
+            return self.forward
+        except Exception, e:
+            print e
+            return False
+
+class TheServer:
+    input_list = []
+    channel = {}
+
+    def __init__(self, host, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((host, port))
+        self.server.listen(200)
+
+    def main_loop(self):
+        self.input_list.append(self.server)
+        while 1:
+            time.sleep(delay)
+            ss = select.select
+            inputready, outputready, exceptready = ss(self.input_list, [], [])
+            for self.s in inputready:
+                if self.s == self.server:
+                    self.on_accept()
+                    break
+
+                self.data = self.s.recv(buffer_size)
+                if len(self.data) == 0:
+                    self.on_close()
+                    break
+                else:
+                    self.on_recv()
+
+    def on_accept(self):
+        forward = Forward().start(forward_to[0], forward_to[1])
+        clientsock, clientaddr = self.server.accept()
+        if forward:
+            print clientaddr, "has connected"
+            self.input_list.append(clientsock)
+            self.input_list.append(forward)
+            self.channel[clientsock] = forward
+            self.channel[forward] = clientsock
+        else:
+            print "Can't establish connection with remote server.",
+            print "Closing connection with client side", clientaddr
+            clientsock.close()
+
+    def on_close(self):
+        print self.s.getpeername(), "has disconnected"
+        #remove objects from input_list
+        self.input_list.remove(self.s)
+        self.input_list.remove(self.channel[self.s])
+        out = self.channel[self.s]
+        # close the connection with client
+        self.channel[out].close()  # equivalent to do self.s.close()
+        # close the connection with remote server
+        self.channel[self.s].close()
+        # delete both objects from channel dict
+        del self.channel[out]
+        del self.channel[self.s]
+
+    def on_recv(self):
+        data = self.data
+        # here we can parse and/or modify the data before sending it forward
+        print data
+        self.channel[self.s].send(data)
+
+if __name__ == '__main__':
+        server = TheServer('', 8443)
+        try:
+            server.main_loop()
+        except KeyboardInterrupt:
+            print "Ctrl C - Stopping server"
+            sys.exit(1)
